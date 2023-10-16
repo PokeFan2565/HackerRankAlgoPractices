70d8311210807161cd6be7f316f72bde9a4d65f4
uknownash <47666850+uknownash@users.noreply.github.com>
2023-10-16 09:22:44 +0530

    Update twoDArry.kt
    Easy And Optimise War To Get Diagonal Deffrence

--- twoDArry.kt
+++ twoDArry.kt
@@ -1,32 +1,13 @@
-fun diagonalDifference(a:Array<Array<Int>>):Int {
-    var num1: Int = 0
-    var num2: Int = 0
-    var index:Int = 0
-    for (i in 0..1) {
-        if (i==0){
-            index=0
-        }else{
-            index=a[0].size-1
-        }
+fun diagonalDifference(arr:Array<Array<Int>>):Int {
+    var left_diagoonal: Int = 0
+    var rightDiagonal: Int = 0
+    var n:Int = arr.size-1
 
-        a.forEach {
-
-            if (i == 0) {
-                num1 += it[index]
-                index += 1
-            }else{
-                num2 += it[index]
-                index -= 1
-            }
-
-        }
+    for (i in 0..n){
+        left_diagoonal += arr[i][i]
+        rightDiagonal += arr[i][n-i]
     }
 
-    val def:Int = if (num2>num1){
-        num2-num1
-    }else{
-        num1-num2
-    }
+return abs(left_diagoonal - rightDiagonal)
 
-    return def
 }
